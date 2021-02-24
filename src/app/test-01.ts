59a189ff8b44380793682775d69baca89bd53148
nasriime <mnasr4040@gmail.com>
2021-02-25 00:09:53 +0200

    test1

--- src/app/test-01.ts
+++ src/app/test-01.ts
@@ -5,26 +5,30 @@
  * * Both [monthly_payment] and [late_payment] should print in the template in currency format : $1,234
  */
 
-import { Component, Input,NgModule  } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
     selector : 'ng-app',
     template : `<div>
                     <h2>Loan Details</h2>
-                    <b>Monthly Payment:</b> {{monthly_payment}} <br/>
-                    <b>Late Payment Fee : {{late_payment}}</b> <br/>
+                    <input type='number' [(ngModel)]= 'loan_amount' min='0' />
+                    <b>Monthly Payment:</b> {{loan_amount ? monthly_payment : 'N/A'}} <br/>
+                    <b>Late Payment Fee : {{loan_amount ? late_payment  : 'N/A'}}</b> <br/>
                 </div>`
 })
 export class Test01Component {
 
     loan_amount:number = 1000;
-    monthly_payment:number = 200;
-    late_payment = 10;
+    monthly_payment:number = (this.loan_amount * 2)/100;
+    late_payment = (this.monthly_payment * 2)/100;;
 }
 
 @NgModule({
     imports : [
+        FormsModule,
         RouterModule.forChild([
             {
                 path : "",
